a05c3613db7ad6794656a4ccb79021c319d3b070
bimawp <bimawirya90@gmail.com>
2025-08-11 15:13:46 +0700

    sialand

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import MarkerClusterGroup from '@changey/react-leaflet-markercluster';
-import '@changey/react-leaflet-markercluster/dist/styles.min.css';
+import MarkerClusterGroup from 'react-leaflet-cluster';
 import L from 'leaflet';
-
 import 'leaflet/dist/leaflet.css';
 
-// Fix untuk icon default Leaflet - gunakan CDN atau icon bawaan
+// Override icon default supaya tidak error asset
 delete L.Icon.Default.prototype._getIconUrl;
-// Gabungkan pengaturan ikon baru
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/marker-icon-2x.png',
-  iconUrl: '/marker-icon.png',
-  shadowUrl: '/marker-shadow.png',
+  iconRetinaUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-// Custom icon untuk marker dengan sumber yang dapat diakses
+// Definisi custom icon (optional)
 const customIcon = new L.Icon({
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
@@ -27,37 +30,32 @@ const customIcon = new L.Icon({
 });
 
 export default function Map() {
-  const [sekolah, setSekolah] = useState([]);
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-    setSekolah([
-      { id: 1, name: "SDN Contoh 1", lat: -7.2155, lng: 107.8932, alamat: "Jalan A" },
-      { id: 2, name: "SDN Contoh 2", lat: -7.2170, lng: 107.8900, alamat: "Jalan B" },
-      { id: 3, name: "SMP Contoh 3", lat: -7.2200, lng: 107.8925, alamat: "Jalan C" },
+    setLocations([
+      { id: 1, name: 'SDN Contoh 1', lat: -7.2155, lng: 107.8932, alamat: 'Jalan A' },
+      { id: 2, name: 'SDN Contoh 2', lat: -7.217, lng: 107.89, alamat: 'Jalan B' },
+      { id: 3, name: 'SMP Contoh 3', lat: -7.22, lng: 107.8925, alamat: 'Jalan C' },
     ]);
   }, []);
 
   return (
-      <MapContainer
-        center={[-7.2170, 107.8930]}
-        zoom={13}
-        style={{ height: '100%', width: '100%' }} // ganti dari '100vh'
-        scrollWheelZoom={true}
-      >
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <MarkerClusterGroup>
-        {sekolah.map((sk) => (
-          <Marker key={sk.id} position={[sk.lat, sk.lng]} icon={customIcon}>
-            <Popup>
-              <strong>{sk.name}</strong><br />
-              {sk.alamat}
-            </Popup>
-          </Marker>
-        ))}
-      </MarkerClusterGroup>
-    </MapContainer>
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <MapContainer center={[-7.217, 107.893]} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <TileLayer attribution="&copy; OpenStreetMap contributors" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MarkerClusterGroup>
+          {locations.map((loc) => (
+            <Marker key={loc.id} position={[loc.lat, loc.lng]} icon={customIcon}>
+              <Popup>
+                <strong>{loc.name}</strong>
+                <br />
+                {loc.alamat}
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </MapContainer>
+    </div>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import slices di sini misal
-// import authReducer from './slices/authSlice';
+import authReducer from './slices/authSlice';
+import budgetReducer from './slices/budgetSlice';
+import counterReducer from './slices/counterSlice';
+import facilityReducer from './slices/facilitySlice';
+import schoolReducer from './slices/schoolSlice';
+import uiReducer from './slices/uiSlice';
 
 const store = configureStore({
   reducer: {
-    // auth: authReducer,
-    // tambahkan reducer lain di sini
+    auth: authReducer,
+    budget: budgetReducer,
+    counter: counterReducer,
+    facility: facilityReducer,
+    school: schoolReducer,
+    ui: uiReducer,
   },
 });
 
